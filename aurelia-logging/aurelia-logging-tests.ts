491b4671cecbaa1827feadd234a4eacda8b811d8
Kristian Mandrup <kmandrup@gmail.com>
2015-05-21 09:19:13 +0100

    logging

--- aurelia-logging/aurelia-logging-tests.ts
+++ aurelia-logging/aurelia-logging-tests.ts
@@ -1,2 +1,24 @@
 /// <reference path="lib.d.ts" />
 /// <reference path="aurelia-logging.d.ts" />
+
+var levels = {
+    none: 2;
+    error: 4;
+    warn: 5;
+    info: 6;
+    debug: 7;
+};
+
+function testLogging {
+  var ae = new AggregateError('x', {}, true);
+  var l = getLogger('x');
+  addAppender({});
+  setLevel(3)
+
+  var lg = new Logger('x', 'y');
+  lg.id;
+  lg.debug();
+  lg.info();
+  lg.warn();
+  lg.error();
+}
