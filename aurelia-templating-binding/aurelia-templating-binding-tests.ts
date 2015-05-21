57f6247857922de2f10aaea3f3974aaa6bb3cad6
Kristian Mandrup <kmandrup@gmail.com>
2015-05-21 09:35:30 +0100

    templating

--- aurelia-templating-binding/aurelia-templating-binding-tests.ts
+++ aurelia-templating-binding/aurelia-templating-binding-tests.ts
@@ -1,2 +1,26 @@
 /// <reference path="lib.d.ts" />
 /// <reference path="aurelia-templating-binding.d.ts" />
+
+var resources = [];
+var element = {};
+var info = {};
+var existingInstruction = {};
+var attrName = 'x';
+
+function testSyntaxInterpreter {
+  var si = new SyntaxInterpreter(parser, observerLocator, eventManager);
+  SyntaxInterpreter.inject();
+  si.parser;
+  si.observerLocator;
+  si.eventManager;
+  si.attributeMap;
+  si.language;
+  si.interpret(resources, element, info, existingInstruction);
+  si.handleUnknownCommand(resources, element, info, existingInstruction);
+  si.determineDefaultBindingMode(element, attrName);
+  si.bind(resources, element, info, existingInstruction);
+  si.trigger(resources, element, info);
+  si.delegate(resources, element, info);
+  si.call(resources, element, info, existingInstruction);
+  si.options(resources, element, info, existingInstruction);
+}
